9e8fdc835b9160f4a8fb9d152ac2638643c02d80
Brian Atiyeh <btatiyeh@gmail.com>
2020-05-08 19:48:46 -0400

    Added menu bar icon & click event

--- MagicBar/AppDelegate.swift
+++ MagicBar/AppDelegate.swift
@@ -11,29 +11,30 @@ import SwiftUI
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
     var window: NSWindow!
-
-
+    var popover: NSPopover!
+    var statusBarItem: NSStatusItem!
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView()
-
-        // Create the window and set the content view. 
-        window = NSWindow(
-            contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
-            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
-            backing: .buffered, defer: false)
-        window.center()
-        window.setFrameAutosaveName("Main Window")
-        window.contentView = NSHostingView(rootView: contentView)
-        window.makeKeyAndOrderFront(nil)
+        createApp()
     }
-
+    
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
+    
+    func createApp() {
+        self.statusBarItem = NSStatusBar.system.statusItem(withLength: CGFloat(NSStatusItem.variableLength))
+        if let button = self.statusBarItem.button {
+            button.image = NSImage(named: "MagicBarIcon")
+            button.action = #selector(connectToMagicMouse(_:))
+        }
+    }
+    
+    @objc func connectToMagicMouse(_ sender: AnyObject?) {
+        if let button = self.statusBarItem.button {
+            print("hello world")
+        }
+    }
 }
 
