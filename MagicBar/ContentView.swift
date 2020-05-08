f71d78b29649d01e2f10c39b4ed13457a601a60f
Brian Atiyeh <btatiyeh@gmail.com>
2020-05-08 16:00:40 -0400

    Initial Commit

---
+++ MagicBar/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MagicBar
+//
+//  Created by Brian Atiyeh on 5/8/20.
+//  Copyright © 2020 Brian Atiyeh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
